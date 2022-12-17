1fb68939aabad7c68f1c444aaa129c6875b368ed
ederushev <118970733+ederushev@users.noreply.github.com>
2022-12-17 18:54:56 +0300

    sprint_04

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,12 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypGreen: UIColor {
+        return UIColor(named: "YP Green (iOS)") ?? UIColor.green
+    }
+    
+    static var ypRed: UIColor {
+        return UIColor(named: "YP Red (iOS)") ?? UIColor.red
+    }
+}
